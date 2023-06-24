2dbf10c0301c662f4367c8441cab075d7227598f
zachpun <137594220+zachpun@users.noreply.github.com>
2023-06-23 22:56:32 -0700

    Delete amazon-clone-main/amazon-clone-main directory

--- amazon-clone-main/amazon-clone-main/src/components/Star.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { BsStarFill, BsStarHalf, BsStar } from "react-icons/bs";
-
-function Star({ text }) {
-  return text === "BsStarFill" ? <BsStarFill /> : text === "BsStarHalf" ? <BsStarHalf /> : <BsStar />;
-}
-
-export default Star;
